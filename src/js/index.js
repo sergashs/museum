c496d986761ef021f276db07d1e7b3a59c1c8825
sergashs <sergas123456@gmail.com>
2024-03-06 11:25:26 +0200

    add .

---
+++ src/js/index.js
@@ -0,0 +1,19 @@
+
+import '../styles/index.scss';
+
+
+
+
+const isTouchScreen = navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i);
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+});
+
+window.addEventListener('resize', () => {
+
+});
+
+
+
